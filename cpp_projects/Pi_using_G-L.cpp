c378d53aa9fd601b966299153b86610e33fa4126
Stergios Tziahanas <stzia@yahoo.com>
2021-11-16 12:53:13 +0200

    commit

--- cpp_projects/Pi_using_G-L.cpp
+++ cpp_projects/Pi_using_G-L.cpp
@@ -1,65 +1,22 @@
 #include <iostream>
-#include <iomanip>
-#include <string>
+#include <math.h>
+using namespace std; 
 
-using namespace std;
+int k;
 
-double Calculate(long int);
+double getPi(int = k) {
+  double num1, num2=0;
+  const int p = 4;
 
-int main()
-{
-    long int iteration;
+  for(int i = 1; k >= i; i ++) {
+    num1 = pow(-1, i + 1)/(2 * i - 1);
+    num2 = num2 + num1;
+  }
+  return num2 * p;
+}
 
-    while (true)
-    {
-        cout << "Welcome to Pi Calculating Program!\n"
-                "Today we will be utilizing the Gregory-Leibniz series to calculate pi.\n";
-        cout << "======================================================================";
-        cout << endl
-             << endl
-             << endl;
-        cout << "Please enter the number of iterations you would like to calculate (-999 to Quit): ";
-        cin >> iteration;
-
-        if (iteration == -999)
-        {
-            cout << "======================================\n";
-            cout << "Thank You for calculating Pi, GoodBye!";
-            cout << "\n======================================\n";
-            return 0;
-        }
-
-        if (!cin || cin.peek() != '\n')
-        {
-            cout << endl
-                 << endl
-                 << endl;
-            cout << "================================================\n";
-            cout << "Invalid input.. Try Again . . .";
-            cout << "\n================================================";
-            cout << endl
-                 << endl
-                 << endl;
-            cin.clear();
-            cin.ignore(32768, '\n');
-            continue;
-
-            Calculate(iteration);
-
-            return 0;
-        } //if
-    }     //while (true)
-} //main
-
-double Calculate(long int iteration)
-{
-
-    double pi;
-    for (int i = 1; i <= iteration; i++)
-    {
-
-        pi = ((4 / i) - (4 / i + 2));
-        i + 4;
-        cout << pi;
-    }
+int main() {
+  cout << "Iterations: ";
+  cin >> k;
+  cout << "Pi is approximated to be " << getPi(k);
 }
